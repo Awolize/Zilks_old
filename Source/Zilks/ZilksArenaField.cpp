fb6efb22da09764117dafc7bc78251bc7bfe7800
Alexander Eriksson <aleer778@student.liu.se>
2021-12-29 21:35:53 +0100

    Added actor which determines the playing field (arena)

---
+++ Source/Zilks/ZilksArenaField.cpp
@@ -0,0 +1,97 @@
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+
+
+#include "ZilksArenaField.h"
+#include "Zilks.h"
+#include "Components/InstancedStaticMeshComponent.h"
+#include "Components/SceneComponent.h"
+
+// Sets default values
+AZilksArenaField::AZilksArenaField()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	RoomSize = 5;
+	PathWidth = 3;
+	bRebuild = true;
+
+	USceneComponent* SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+	SetRootComponent(SceneComponent);
+
+	Floors = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Floors"));
+	Floors->SetupAttachment(SceneComponent);
+
+	Bases = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Bases"));
+	Bases->SetupAttachment(SceneComponent);
+
+}
+
+#if WITH_EDITOR
+void AZilksArenaField::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	if (PropertyChangedEvent.Property && PropertyChangedEvent.Property->HasMetaData("RebuildArenaField"))
+		bRebuild = true;
+
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+}
+#endif
+
+
+void AZilksArenaField::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	// Only rebuild if needed.
+	if (!bRebuild)
+		return;
+
+	bRebuild = false;
+
+
+	UE_LOG(LogZilks, Log, TEXT("AZilksArenaField::OnConstruction Building Room Size %d (this=%x)"), RoomSize, this);
+
+
+	Floors->ClearInstances();
+	Bases->ClearInstances();
+
+
+
+	// Implicit floor with integer division, which makes all room sizes end up being odd.
+	int32 HalfPathWidth = PathWidth / 2;
+	int32 HalfSize = RoomSize / 2;
+	int32 GridSize = 410;
+
+	FVector FloorTranslation(0.f, 0.f, 0.f);
+
+	for (int32 x = -HalfSize; x <= HalfSize; x++)
+	{
+		FloorTranslation.X = GridSize * x;
+
+		for (int32 y = -HalfPathWidth; y <= HalfPathWidth; y++)
+		{
+			FloorTranslation.Y = GridSize * y;
+			Floors->AddInstance(FTransform(FloorTranslation));
+
+			/*
+			 * Calculate where to place the Bases, should be in the middle on each short-side
+			 */
+
+			FRotator Rotation(0.f, 90.f, 0.f);
+			FVector Translation = FloorTranslation;
+
+			if (y == 0 && abs(x) == HalfSize) // if in the middle of the grid (on the y axis)
+			{
+				if (x == HalfSize) {
+					float FloorInstanceSize = Floors->GetStaticMesh()->GetBounds().BoxExtent.X * 2; // get Floor mesh size to offset the placement with it. * 2 to get the entire size and not just half
+					Translation.X += FloorInstanceSize;
+				}
+
+				Bases->AddInstance(FTransform(Rotation, Translation));
+			}
+		}
+	}
+
+
+
+
+
+}
---
+++ Source/Zilks/ZilksArenaField.h
@@ -0,0 +1,45 @@
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ZilksArenaField.generated.h"
+
+UCLASS()
+class ZILKS_API AZilksArenaField : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AZilksArenaField();
+
+#if WITH_EDITOR
+	/** Check properties that change to see if we need to rebuild. */
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	/** Static mesh to use for the 2 player bases. */
+	UPROPERTY(EditAnywhere)
+	class UInstancedStaticMeshComponent* Bases;
+
+	/** Static mesh to use for walls. */
+	UPROPERTY(EditAnywhere)
+	class UInstancedStaticMeshComponent* Floors;
+
+	/** Size of grid to use when placing meshes. */
+	UPROPERTY(EditAnywhere, meta = (ClampMin = 3, ClampMax = 25, RebuildArenaField))
+	uint32 RoomSize;
+
+	/** Size of grid to use when placing meshes. */
+	UPROPERTY(EditAnywhere, meta = (ClampMin = 1, ClampMax = 5, RebuildArenaField))
+	uint32 PathWidth;
+
+
+private:
+	/** Whether we need to rebuild or not. */
+	int32 bRebuild : 1;
+};
